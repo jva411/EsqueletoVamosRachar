6f20d5ca0e5b867efd3d80fd26d45df3a0fb3591
jva411 <j.victoralves411@gmail.com>
2023-10-10 17:15:42 -0300

    fazendo o compartilhamento e o TTS

--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.constraintlayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
@@ -8,6 +9,7 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
@@ -27,7 +29,40 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         findViewById<EditText>(R.id.inputPessoas).addTextChangedListener {
             updateConta(false, it.toString())
         }
+        findViewById<FloatingActionButton>(R.id.btnPlay).setOnClickListener {
+            val preco = this.conta / this.pessoas
+            val reais = preco.toInt()
+            val centavos = ((preco - reais) * 100).toInt()
 
+            val texto = when {
+                isSomeInputEmpty() -> "Preencha todos os campos"
+                this.error -> "Algum erro aconteceu durante os cálculos"
+                centavos == 0 -> "Cada um deve pagar $reais reais"
+                else -> "Cada um deve pagar $reais reais e $centavos centavos"
+            }
+            tts.speak(texto, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+        findViewById<FloatingActionButton>(R.id.btnShare).setOnClickListener {
+            if (isSomeInputEmpty()) {
+                tts.speak("Preencha todos os campos", TextToSpeech.QUEUE_FLUSH, null, null)
+                return@setOnClickListener
+            } else if (this.error) {
+                tts.speak("Algum erro aconteceu durante os cálculos", TextToSpeech.QUEUE_FLUSH, null, null)
+                return@setOnClickListener
+            }
+
+            val preco = this.conta / this.pessoas
+            val texto = getString(R.string.app_name) + "\n" +
+                    "Cada um deve pagar R$%.2f".format(preco)
+
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, texto)
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
     }
 
     private fun updateConta(isConta: Boolean, value: String) {
@@ -41,8 +76,14 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         } catch (ex: Exception) {
             this.error = true
         } finally {
+            if (this.pessoas == 0) {
+                this.error = true
+                findViewById<TextView>(R.id.tvConta).text = "R$0,00"
+                return
+            }
+
             val preco = this.conta / this.pessoas
-            findViewById<TextView>(R.id.tvConta).text = if(isSomeInputEmpty()) "R$0,00" else "R$.2f".format(preco)
+            findViewById<TextView>(R.id.tvConta).text = if(isSomeInputEmpty()) "R$0,00" else "R$%.2f".format(preco)
         }
     }
 
@@ -51,9 +92,6 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 findViewById<EditText>(R.id.inputConta).text.isEmpty()
     }
 
-    fun clickFalar(v: View){
-        tts.speak("Oi Sumido", TextToSpeech.QUEUE_FLUSH, null, null)
-    }
     override fun onDestroy() {
             // Release TTS engine resources
             tts.stop()
